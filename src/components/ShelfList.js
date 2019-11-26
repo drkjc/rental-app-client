8e784937e838a939c71d3f7408bcab50eada35f5
Derek Cerretani <drkjc@protonmail.com>
2019-11-26 07:56:55 -0700

    shelfList returns if/else to prevent errors

--- src/components/ShelfList.js
+++ src/components/ShelfList.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ShelfCard from './ShelfCard';
 
 const ShelfList = ({ shelves, user_id }) => {
-  return shelves.map(shelf => {
-    if (shelf.user.id !== user_id) {
-      return <ShelfCard key={shelf.id} shelf={shelf} />
-    }
-  }) 
+  if(!shelves) {
+    return <div>Loading...</div>
+  } else {
+    return shelves.map(shelf => {
+      if (shelf.user.id !== user_id) {
+        return <ShelfCard key={shelf.id} shelf={shelf} />
+      }
+    })
+  } 
 }
 
 export default ShelfList;
