75a773b6faba9c809c2214d9395d91736614ee17
drkjc <drkjc@protonmail.com>
2020-03-21 12:14:42 -0700

    create AddItemForm.css & UserShelf.css / transfer css from app to new files

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -1,42 +1,64 @@
-import React, { Component } from  'react'; 
-class AddItemForm extends Component {
+import React, { Component } from "react";
+import "../css/AddItemForm.css";
 
+class AddItemForm extends Component {
   state = {
-    name: '',
-    category: '',
-    price: ''
-  }
+    name: "",
+    category: "",
+    price: ""
+  };
 
   handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
-    })
-  }
+    });
+  };
 
   handleSubmit = event => {
     event.preventDefault();
-    let user_id = this.props.user_id 
-    let shelf_id = user_id
-    this.props.addItem(user_id, shelf_id, this.state)
+    let user_id = this.props.user_id;
+    let shelf_id = user_id;
+    this.props.addItem(user_id, shelf_id, this.state);
     this.setState({
-      name: '',
-      category: '',
-      price: ''
-    })
-  }
+      name: "",
+      category: "",
+      price: ""
+    });
+  };
 
   render() {
     return (
-      <div id='addItemForm'>
+      <div id="addItemForm">
         <form onSubmit={this.handleSubmit}>
-          <input type="text" name="name" placeholder="name" value={this.state.name} onChange={this.handleChange}></input><br/>
-          <input type="text" name="category" placeholder="category" value={this.state.category} onChange={this.handleChange}></input><br />
-          <input type="text" name="price" placeholder="price" value={this.state.price} onChange={this.handleChange}></input><br />
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          ></input>
+          <br />
+          <input
+            type="text"
+            name="category"
+            placeholder="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          ></input>
+          <br />
+          <input
+            type="text"
+            name="price"
+            placeholder="price"
+            value={this.state.price}
+            onChange={this.handleChange}
+          ></input>
+          <br />
           <button>Add Item</button>
         </form>
       </div>
-    )
-  } 
+    );
+  }
 }
 
 export default AddItemForm;
--- src/containers/UserShelf.js
+++ src/containers/UserShelf.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
-import { getUserShelfItems, addItemToShelf } from '../redux/actions/shelf';
-import { connect } from 'react-redux';
-import UserShelfItemList from '../components/UserShelfItemList'
-import AddItemForm from '../components/AddItemForm';
+import React, { Component } from "react";
+import { getUserShelfItems, addItemToShelf } from "../redux/actions/shelf";
+import { connect } from "react-redux";
+import UserShelfItemList from "../components/UserShelfItemList";
+import AddItemForm from "../components/AddItemForm";
+import "../css/UserShelf.css";
 
 class UserShelf extends Component {
-
   componentDidMount() {
     // hard coded user_id, and shelf_id
-    let user_id = this.props.user_id
-    let shelf_id = user_id
+    let user_id = this.props.user_id;
+    let shelf_id = user_id;
     this.props.getUserShelfItems(user_id, shelf_id);
   }
 
   handleLoading = () => {
     if (this.props.loading) {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     } else {
-      return <UserShelfItemList items={this.props.items} />
+      return <UserShelfItemList items={this.props.items} />;
     }
-  }
-
+  };
 
   render() {
     return (
       <div id="user-shelf">
-        <AddItemForm addItem={this.props.addItemToShelf} user_id={this.props.user_id}/>
+        <AddItemForm
+          addItem={this.props.addItemToShelf}
+          user_id={this.props.user_id}
+        />
         {this.handleLoading()}
       </div>
-    )
+    );
   }
 }
 
@@ -36,7 +38,9 @@ const mapStateToProps = state => {
   return {
     items: state.shelf.items,
     loading: state.shelf.loading
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, { getUserShelfItems, addItemToShelf })(UserShelf)
+export default connect(mapStateToProps, { getUserShelfItems, addItemToShelf })(
+  UserShelf
+);
