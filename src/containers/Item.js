664467b8f094ecd1dc41cb6dc03671f8281234ff
drkjc <drkjc@protonmail.com>
2020-03-21 12:10:27 -0700

    create Item.css transfer CSS from app to item

--- src/containers/Item.js
+++ src/containers/Item.js
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
-import { getShelfItems } from '../redux/actions/shelf';
-import { connect } from 'react-redux';
-import ItemList from '../components/ItemList';
-import Ad from '../components/Ad';
+import React, { Component } from "react";
+import { getShelfItems } from "../redux/actions/shelf";
+import { connect } from "react-redux";
+import ItemList from "../components/ItemList";
+import Ad from "../components/Ad";
+import "../css/Item.css";
 
 class Item extends Component {
-
   componentDidMount() {
-    console.log(this.props, 'Item')
-    let shelf_id = this.props.match.params.shelf_id
+    console.log(this.props, "Item");
+    let shelf_id = this.props.match.params.shelf_id;
     this.props.getShelfItems(shelf_id);
   }
 
   handleLoading = () => {
     if (this.props.loading) {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     } else {
-      return <ItemList items={this.props.items} />
+      return <ItemList items={this.props.items} />;
     }
-  }
-
+  };
 
   render() {
     return (
       <>
-        <div id="main-content"><Ad /></div>
+        <div id="main-content">
+          <Ad />
+        </div>
         <div id="shelf">
-        <h3>Items</h3>
-        {this.handleLoading()}
+          <h3>Items</h3>
+          {this.handleLoading()}
         </div>
       </>
-    )
+    );
   }
 }
 
@@ -38,7 +39,7 @@ const mapStateToProps = state => {
   return {
     items: state.shelf.items,
     loading: state.shelf.loading
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, { getShelfItems })(Item)
+export default connect(mapStateToProps, { getShelfItems })(Item);
