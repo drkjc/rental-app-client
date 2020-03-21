7d8dd088e817bbd17a6c50ff766710df398fe40c
drkjc <drkjc@protonmail.com>
2020-03-21 12:11:47 -0700

    create EditItemForm.css / transfer css from app to editItemForm

--- src/components/EditItemForm.js
+++ src/components/EditItemForm.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom'
-import { updateItem } from '../redux/actions/shelf'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { updateItem } from "../redux/actions/shelf";
+import "../css/EditItemForm.css";
 
 class EditItemForm extends Component {
-
   state = {
-    id: '',
-    name: '',
-    category: '',
-    price: '',
+    id: "",
+    name: "",
+    category: "",
+    price: "",
     redirect: false
-  }
+  };
 
   componentDidMount() {
     this.setState({
@@ -19,45 +19,64 @@ class EditItemForm extends Component {
       name: this.props.location.state.name,
       category: this.props.location.state.category,
       price: this.props.location.state.price
-    })
+    });
   }
 
   handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
-    })
-  }
+    });
+  };
 
   handleSubmit = event => {
     event.preventDefault();
-    let user_id = this.props.user_id
-    let shelf_id = user_id
-    this.props.updateItem(user_id, shelf_id, this.state)
-    setInterval(() => { 
+    let user_id = this.props.user_id;
+    let shelf_id = user_id;
+    this.props.updateItem(user_id, shelf_id, this.state);
+    setInterval(() => {
       this.setState({
         redirect: true
-      })}, 1750);
-  }
+      });
+    }, 1750);
+  };
 
   renderRedirect = () => {
     if (this.state.redirect) {
-      return <Redirect to='/shelf' />
+      return <Redirect to="/shelf" />;
     }
-  }
+  };
 
   render() {
-    console.log(this.state, 'edit form')
+    console.log(this.state, "edit form");
     return (
       <div>
         {this.renderRedirect()}
         <form onSubmit={this.handleSubmit} id="editItemForm">
-          <input type="text" name="name" value={this.state.name} onChange={this.handleChange}></input><br />
-          <input type="text" name="category" value={this.state.category} onChange={this.handleChange}></input><br />
-          <input type="text" name="price" value={this.state.price} onChange={this.handleChange}></input><br />
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          ></input>
+          <br />
+          <input
+            type="text"
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          ></input>
+          <br />
+          <input
+            type="text"
+            name="price"
+            value={this.state.price}
+            onChange={this.handleChange}
+          ></input>
+          <br />
           <button>Edit Item</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
